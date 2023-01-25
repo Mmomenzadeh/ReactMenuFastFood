6ddca525fba53196a0efad128432fd462d29adaf
Mmomenzadeh <m.momenzadeh1999@gmail.com>
2023-01-25 22:38:23 +0330

    add font

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 import "./assets/styles/App.css"
 
-const App =()=>{}
+const App =()=>{
+    return(
+        <div className="container">
+            
+        </div>
+    )
+}
 
 export default App
